f01a8258bf187eff11cec2b5c4a3736df5dd4dac
NgocTrungTran <milotran1496@gmail.com>
2022-07-01 14:46:11 +0700

    TH Su dung cac ham

---
+++ CacHamThongDung/THCacHamThongDung.sql
@@ -0,0 +1,19 @@
+use quanlysinhvien;
+-- Su dung ham COUNT de hien thi so luong sinh vien o tung noi
+SELECT Address, COUNT(StudentID) AS 'Số lượng học viên'
+FROM Student 
+GROUP BY Address;
+-- Tính điểm trung bình các môn học của mỗi học viên bằng cách sử dụng hàm AVG
+SELECT S.StudentID, S.StudentName, AVG(Mark)
+FROM Student S JOIN Mark M ON S.StudentId = M.StudentId
+GROUP BY S.StudentId, S.StudentName;
+-- Hiển thị những bạn học viên co điểm trung bình các môn học lớn hơn 15
+SELECT S.StudentID, S.StudentName, AVG(Mark)
+FROM Student S JOIN Mark M ON S.StudentId = M.StudentId
+GROUP BY S.StudentId, S.StudentName
+HAVING AVG(Mark) >= 15;
+-- Hiển thị thông tin các học viên có điểm trung bình lớn nhất.
+SELECT S.StudentID, S.StudentName, AVG(Mark)
+FROM Student S JOIN Mark M ON S.StudentId = M.StudentId
+GROUP BY S.StudentId, S.StudentName
+HAVING AVG(Mark) >= ALL (SELECT AVG(Mark) FROM Mark GROUP BY Mark.StudentId);
