7c801d53389501ac113cc78e47bb576b57a0031d
NgocTrungTran <milotran1496@gmail.com>
2022-07-08 23:04:59 +0700

    TH Tao thoi gian hien tai bang Servlet

---
+++ Servlet/THServletTimeSystem/src/main/java/milo/org/thservlettimesystem/HelloServlet.java
@@ -0,0 +1,29 @@
+package milo.org.thservlettimesystem;
+
+import java.io.*;
+import java.util.Date;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet-test")
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello World!";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType ( "text/html" );
+
+        // Hello
+        PrintWriter writer = response.getWriter ();
+        writer.print ( "<html>" );
+        Date today = new Date ();
+        writer.println ( "<h1>" + today + "</h1>" );
+        writer.println ( "</html>" );
+    }
+
+    public void destroy() {
+    }
+}
